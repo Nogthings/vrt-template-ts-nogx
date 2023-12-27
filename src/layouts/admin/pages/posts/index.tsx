c2c54a2f733dcbb0ec5d9dd5b983be8524f13b3e
nogthings <oscar@doyenx.com>
2023-12-26 19:51:14 -0700

    Add IconButton component to Posts page

--- src/layouts/admin/pages/posts/index.tsx
+++ src/layouts/admin/pages/posts/index.tsx
@@ -1,6 +1,7 @@
 import Button from "../../../components/buttons/button";
 import { useNavigate } from "react-router-dom";
 import { IoAddOutline } from "react-icons/io5";
+import IconButton from "../../../components/buttons/iconButton";
 function Posts() {
   const navigate = useNavigate();
   const handleCreateClick = () => {
@@ -9,11 +10,14 @@ function Posts() {
 
   return (
     <div className="h-screen flex gap-2">
-      <div>
+      <div className="space-y-2">
         <h1>Posts</h1>
         <Button title="Create Post" onClick={handleCreateClick}>
           <IoAddOutline />
         </Button>
+        <IconButton rounded color="#89608E">
+          <IoAddOutline />
+        </IconButton>
       </div>
     </div>
   );
--- src/layouts/components/buttons/iconButton/index.tsx
+++ src/layouts/components/buttons/iconButton/index.tsx
@@ -5,10 +5,10 @@ function IconButton({ children, onClick, color, rounded }: IconButtonProps) {
     <button
       onClick={onClick}
       className={`flex items-center justify-center aspect-square ${
-        color ? `${color}` : "bg-purple-400"
+        color ? `bg-[${color}]` : "bg-purple-400"
       } ${
-        rounded ? "" : "rounded-full"
-      } transition-all duration-100 hover:bg-purple-300`}
+        rounded ? "rounded-full" : ""
+      } transition-all duration-100 hover:bg-purple-300 p-2 text-white`}
     >
       {children}
     </button>
