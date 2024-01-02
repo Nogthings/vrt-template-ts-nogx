dc802774db63c4ff8ba55ddef8ebea15a7c2dce7
nogthings <oscar@doyenx.com>
2024-01-02 02:58:45 -0700

    Fix key warning in Breadcrumb component

--- src/layouts/admin/components/breadcrumb/index.tsx
+++ src/layouts/admin/components/breadcrumb/index.tsx
@@ -25,7 +25,7 @@ function Breadcrumb() {
     <div aria-label="Breadcrumb">
       <ol className="flex items-center gap-1 text-sm text-dark-600 dark:text-dark-100">
         {breadcrumbs.map((breadcrumb, index) => (
-          <div className="flex items-center gap-1">
+          <div className="flex items-center gap-1" key={index}>
             <li key={breadcrumb.name}>
               {breadcrumb.path === "/admin" ? (
                 <Link
@@ -44,7 +44,7 @@ function Breadcrumb() {
               )}
             </li>
             {index < breadcrumbs.length - 1 && (
-              <li key={index}>
+              <li key={`chevron-${index}`}>
                 <IoChevronForwardOutline />
               </li>
             )}
